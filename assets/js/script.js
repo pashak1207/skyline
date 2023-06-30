5eb27495d5c162711aab571f92012f78e4abc689
pashak1207 <pashak12072002@gmail.com>
2023-06-30 15:00:00 +0300

    Home page changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,35 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //END HEADER ONSCROLL//
 
-  //ADVANTAGES SLIDER//
-  const slider = document.querySelector('.advantages__items');
-  let isDown = false;
-  let startX;
-  let scrollLeft;
-
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 2;
-    slider.scrollLeft = scrollLeft - walk;
-  });
-  //END ADVANTAGES SLIDER//
-
   //QUESTIONS SELECT//
   const variants = document.querySelectorAll(".questions__item")
 
